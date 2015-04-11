b603aee04a3d34bb27122f2f98c9b7b3de8d5178
joshWyatt <joshuawyatt123@gmail.com>
2015-04-11 13:49:29 -0700

    Initial commit; prepare for testing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # print_usage
+
+Exit the program and print the usage from usage.md or a provided string.
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+var test = require('tape');
+
+test('index.js', function(t){
+  t.plan(1);
+
+  t.pass('it should run without an error');
+});
