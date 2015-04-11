a5641d59a382dbccb3853673dc87f5cf3fbc6f18
joshWyatt <joshuawyatt123@gmail.com>
2015-04-11 14:47:36 -0700

    Tear down test for now

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var fileExists = require('file-exists');
+
+module.exports = printUsage;
+
+function printUsage (usage) {
+  if (usage) {
+    console.log(usage);
+    process.exit();
+  }
+  if (fileExists(usage = process.cwd() + '/usage.md')) {
+    var rs = fs.createReadStream(usage);
+    rs.pipe(process.stdout);
+    rs.on('end', function() {
+      process.exit();
+    });
+  }
+  else {
+    throw new Error('----> there is neither a usage string or usage.md file in the cwd!');
+  }
+}
--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-var test = require('tape');
-
-test('index.js', function(t){
-  t.plan(1);
-
-  t.pass('it should run without an error');
-});
